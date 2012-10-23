5e3bb7ec4d29ec5db2956e23a729b3de8aeb5fbd
David Perit <dperit@gmail.com>
2012-10-23 17:10:02 -0400

    Included additional states in the state enum Fixed various warnings related to not returning ints Added a find characters before cues that figures out if comment/garbage characters have been included before the cues Added a skip characters before cues function that skips past those characters if they are present Added shell of a skip_line_terminators before cue function that will skip past the line terminators that have to be included before each cue added a cue link function that links cues together added cases for the new states that call the new functions and then advance the state added additional parameters to the parse function as well as additional functions that prevent legacy calls from breaking

--- webvtt.c
+++ webvtt.c
@@ -20,6 +20,15 @@
 #define MIN(x, y) (((x) < (y)) ? (x) : (y))
 #endif
 
+enum state {
+BOM = 0,
+SIGNATURE = 1,
+FIND_CHARACTERS_BEFORE_CUES = 2,
+SKIP_CHARACTERS_BEFORE_CUES = 3,
+SKIP_LINE_TERMINATORS_BEFORE_CUE = 4,
+CUE = 5
+};
+
 struct webvtt_parser {
   int parse_state;
   int reached_buffer_end;
@@ -28,11 +37,6 @@ struct webvtt_parser {
   char *buffer;
   long offset, length;
 };
-
-enum parse_states {
-BOM: 0,
-SIGNATURE: 1,  
-}
 
 webvtt_parser *
 webvtt_parse_new(void)
@@ -169,10 +173,9 @@ webvtt_parse_cue(webvtt_parser *ctx)
 int webvtt_parse_byte_order_mark(webvtt_parser *ctx){
   if (ctx->length < 3){
     ctx->reached_buffer_end = 1;
+    return -1;
   }else {
     char *p = ctx->buffer;
-    //We'll be going ahead to the next stage if it has a BOM or not
-    ctx->parse_state++;
     //Assuming that this set of hexcodes == U+FEFF BYTE ORDER MARK (BOM)
     if (p[0] == (char)0xef && p[1] == (char)0xbb && p[2] == (char)0xbf){
       ctx->offset += 3;
@@ -190,13 +193,14 @@ int webvtt_parse_signature(webvtt_parser *ctx){
   if (!ctx->has_BOM && ctx->length < 6) {
     ctx->reached_buffer_end = 1;
     fprintf(stderr, "Too short. Not capable of parsing signature yet\n");
+    return -1;
   }else if (ctx->has_BOM && ctx->length < 9) {
     ctx->reached_buffer_end = 1;
     fprintf(stderr, "Too short. Not capable of parsing signature yet\n");
+    return -1;
   }else {
-    if (memcmp(ctx->buffer[ctx->offset], "WEBVTT", 6)) {
+    if (memcmp(ctx->buffer + ctx->offset, "WEBVTT", 6)) {
       ctx->offset += 6;
-      ctx->parse_state = 2;
       return 1;
     }else {
       ctx->invalid_buffer = 1;
@@ -205,15 +209,61 @@ int webvtt_parse_signature(webvtt_parser *ctx){
   }
 }
 
-int webvtt_skip_whitespace(webvtt_parser *ctx){
-  //TODO: Make sure that 
-  while (ctx->offset < ctx->length && isspace(ctx->buffer[ctx->offset]))
+//Optionally, either a U+0020 SPACE character or a U+0009 CHARACTER TABULATION (tab) character 
+//followed by any number of characters that are not U+000A LINE FEED (LF) or 
+//U+000D CARRIAGE RETURN (CR) characters.
+int webvtt_find_characters_before_cues(webvtt_parser *ctx){
+  if (ctx->offset < ctx->length){
+    if (ctx->buffer[ctx->offset] == (char)0x20 || ctx->buffer[ctx->offset] == (char)0x9){
+      ctx->offset++;
+      return 1;
+    }else {
+      return 0;
+    }
+  }else {
+    ctx->reached_buffer_end = 1;
+    return -1;
+  }
+}
+
+void webvtt_skip_characters_before_cues(webvtt_parser *ctx){
+  while (ctx->offset < ctx->length && (ctx->buffer[ctx->offset] != (char)0xA
+                                       && ctx->buffer[ctx->offset] != (char)0xD)){
     ctx->offset++;
-  
+  }
+  if (ctx->offset == ctx->length){
+    //Don't retreat the offset or change the parse state if we hit the end during whitespace 
+    //just indicate we've reached the end. That way when we run through again with more buffer
+    //we'll just run through this method again until the whitespace ends
+    ctx->reached_buffer_end = 1;
+  }  
+}
+
+//A WebVTT line terminator consists of one of the following:
+
+//A U+000D CARRIAGE RETURN U+000A LINE FEED (CRLF) character pair.
+//A single U+000A LINE FEED (LF) character.
+//A single U+000D CARRIAGE RETURN (CR) character.
+void webvtt_skip_line_terminators_before_cue(webvtt_parser *ctx){
+  //TODO: Implement this!
+  //if (ctx->offset < ctx->length){
+//  }else {
+//    ctx->reached_buffer_end = 1;
+//  }
+
+}
+
+webvtt_cue * webvtt_cue_link(webvtt_cue *to_link, webvtt_cue *to_link_from){
+  if (to_link && to_link_from) {
+    to_link_from->next = to_link;
+    return to_link;
+  }else {
+    return to_link_from;
+  }
 }
 
 webvtt_cue *
-webvtt_parse(webvtt_parser *ctx)
+webvtt_parse(webvtt_parser *ctx, webvtt_cue *first_cue, webvtt_cue* last_cue)
 {
   webvtt_cue *cue = NULL;
   
@@ -228,34 +278,77 @@ webvtt_parse(webvtt_parser *ctx)
         //parser to be re-run once more file is available.
         //If it finds something indicating that the provided file is not valid then it should set the
         //invalid_buffer flag to 1.
-      case 0:
-          webvtt_parse_byte_order_mark(ctx);
-          break;
-        case 1:
+      case BOM:
+        webvtt_parse_byte_order_mark(ctx);
+        if (!ctx->reached_buffer_end){
+          ctx->parse_state = SIGNATURE;
+        }
+        break;
+      case SIGNATURE:
         webvtt_parse_signature(ctx);
-          break;
-    
-    webvtt_skip_whitespace(ctx);
-    
-    cue = webvtt_parse_cue(ctx);
-    if (cue) {
-      webvtt_cue *head = cue;
-      webvtt_cue *next;
-      do {
-        next = webvtt_parse_cue(ctx);
-        head->next = next;
-        head = next;
-      } while (next != NULL);
-    }
-    
-    webvtt_cue *head = cue;
-    while (head != NULL) {
-      webvtt_print_cue(stderr, head);
-      head = head->next;
+        if (!ctx->reached_buffer_end && !ctx->invalid_buffer){
+          ctx->parse_state = FIND_CHARACTERS_BEFORE_CUES;
+        }
+        break;
+      case FIND_CHARACTERS_BEFORE_CUES:
+        //Gives the compiler a scope to put has_characters_before_cues in
+        if(1){
+          int has_characters_before_cues = webvtt_find_characters_before_cues(ctx);
+          if (!ctx->reached_buffer_end){
+            if (has_characters_before_cues){
+              ctx->parse_state = SKIP_CHARACTERS_BEFORE_CUES;
+            }else {
+              ctx->parse_state = SKIP_LINE_TERMINATORS_BEFORE_CUE;
+            }
+          }
+        }
+        break;
+      case SKIP_CHARACTERS_BEFORE_CUES:
+        webvtt_skip_characters_before_cues(ctx);
+        if (!ctx->reached_buffer_end){
+          ctx->parse_state = SKIP_LINE_TERMINATORS_BEFORE_CUE;
+        }
+        break;
+      case SKIP_LINE_TERMINATORS_BEFORE_CUE:
+        webvtt_skip_line_terminators_before_cue(ctx);
+        if (!ctx->reached_buffer_end && !ctx->invalid_buffer){
+          ctx->parse_state = CUE;
+        }
+        break;
+      case CUE:
+        cue = webvtt_parse_cue(ctx);
+        if (!ctx->reached_buffer_end && !ctx->invalid_buffer){
+          if (first_cue == NULL){
+            first_cue = cue;
+            last_cue = cue;
+          }
+          last_cue = webvtt_cue_link(cue, last_cue);
+        }
+        break;
     }
   }
-
+  webvtt_cue *head = cue;
+  while (head != NULL) {
+    webvtt_print_cue(stderr, head);
+    head = head->next;
+  }
   return cue;
+}
+
+webvtt_cue * webvtt_parse(webvtt_parser *ctx, webvtt_cue *first_cue){
+  webvtt_cue *last_cue = first_cue;
+  //Find out what the last cue is so we can send it into the parser
+  if (first_cue != NULL){
+    while (last_cue->next != NULL){
+      last_cue = last_cue->next;
+    }
+  }
+  return webvtt_parse(ctx, first_cue, last_cue);
+}
+
+webvtt_cue * webvtt_parse(webvtt_parser *ctx){
+  webvtt_cue *first_cue = NULL;
+  return webvtt_parse(ctx, first_cue);
 }
 
 webvtt_cue *
